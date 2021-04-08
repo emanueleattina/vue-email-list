f21cb9f7e4faebfb997234428432b8b53d2fe4b4
emanuele <emattina97@gmail.com>
2021-04-08 16:17:56 +0200

    Carico esercizio

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-var app = new Vue ({
+var app   = new Vue ({
     el: '#root',
     data: {
-        
+        email: []
     },
+    methods: {
+        mailGenerator: function () {
+            for(i = 0; i < 10; i ++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.email.push(response.data.response);
+                });  
+            }
+        },
+    }
 });
