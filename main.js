02fdc8a234d8c90c536b0b1b5057912bdffdfb5e
emanuele <emattina97@gmail.com>
2021-04-08 17:07:15 +0200

    Carico correzione button

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ var app   = new Vue ({
     el: '#root',
     data: {
         email: [],
-        isActive: true
+        isActive: false
     },
     methods: {
         mailGenerator: function () {
+            this.isActive = true;
             for(i = 0; i < 10; i ++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
